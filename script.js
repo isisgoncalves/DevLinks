6836887a359f3792614ff3f3bc9e7d0c92913087
isisgoncalves <isisvow@gmail.com>
2023-09-23 08:53:28 -0300

    alterando img
    Aprendendo a alterar imagem com JS

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function toggleMode() {
   const html= document.documentElement
   html.classList.toggle("light")
+
+  //*Alterando imagem com JS=
+  //*Pegar a tag img:
+  //  const img = document.querySelector("#profile img")
+  //*Substituir a imagem:
+  //  if(html.classList.contains("light")){
+  //*Se tiver light mode, adicionar essa imagem:  
+  //    img.setAttribute("src", "./local da imagem")
+  //*Se não estiver no light mode, manter a imagem normal:
+  //  } else {
+  //    img.setAttribute("src", "./local da imagem")
+  //  }
 }
